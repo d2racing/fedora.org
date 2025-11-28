f05d8ec33c55279f014f70d9f5e69db24e70664f
Sylvain Alain <d2racing911@gmail.com>
2025-11-28 15:16:40 -0500

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -4,7 +4,7 @@
 # ==========================================
 
 # --- CONFIGURATION ---
-NAS_IP="xxx.xxx.x.xxx"
+NAS_IP="xxx.xxx.xxx.xxx"
 CREDENTIALS_FILE="/root/.nas-credentials"  # format : username=XX_XXX / password=YYYY
 
 SHARES=("CLONEZILLA" "DIVERS" "DONNEES" "homes" "LOGICIELS" "photo" "PHOTOSYNC" "STORAGE_ANALYZER")
