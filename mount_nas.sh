10ce30baf7168c5733052681efac27a96c29bfd3
Sylvain Alain <d2racing911@gmail.com>
2025-12-03 18:01:43 -0500

    Update mount_nas.sh

--- mount_nas.sh
+++ mount_nas.sh
@@ -3,6 +3,7 @@ echo 'start'
 # ==========================================
 # Montage du NAS
 # ==========================================
+
 # --- CONFIGURATION ---
 NAS_IP="xxx.xxx.x.xxx"
 CREDENTIALS_FILE="/root/.nas-credentials"  # format : username=XX_XXX / password=YYYY
@@ -14,11 +15,14 @@ for SHARE in "${SHARES[@]}"; do
     SRC="$NAS_MOUNT_BASE/$SHARE"
     echo ">>> Montage de $SHARE..."
     sudo mkdir -p "$SRC"
-    sudo mount -t cifs "//$NAS_IP/$SHARE" "$SRC" \
-        -o credentials="$CREDENTIALS_FILE",rw,iocharset=utf8,vers=3.0,uid=1000,gid=1000 || { echo "Échec du montage de $SHARE"; continue; }
+
+    if ! sudo mount -t cifs "//$NAS_IP/$SHARE" "$SRC" \
+         -o credentials="$CREDENTIALS_FILE",rw,iocharset=utf8,vers=3.0,uid=1000,gid=1000; then
+        
+        echo "❌ Échec du montage de $SHARE"
+        echo "⛔ Arrêt complet du script."
+        exit 1
+    fi
 done
 
-# nano /root/.nas-credential
-#username=...
-#password=...
-# chmod 600 /root/.nas-credential
+echo "✔ Tous les partages ont été montés avec succès."
